92c5b0381d0509ab973892bce377ee6d0f3e8422
Ego-html <62375400+Ego-html@users.noreply.github.com>
2024-08-21 23:44:22 +0300

    add auth

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,35 @@
+// src/app/components/login/login.component.ts
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule]
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    const { username, password } = this.loginForm.value;
+
+    this.authService.login(username, password).subscribe((success: boolean) => {
+      if (success) {
+        this.router.navigate(['/']);
+      } else {
+        alert('Invalid credentials');
+      }
+    });
+  }
+}
